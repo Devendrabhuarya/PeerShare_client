c6864a37c6cec56cadf6bdbac344909b256de2c8
devendrakumar <101959297+Devendrabhuarya@users.noreply.github.com>
2024-04-12 23:36:45 +0530

    Update navbar.tsx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -13,13 +13,13 @@ export default function NavBar() {
     const router = useRouter();
     const { setLoader } = useContext(LoaderContext);
     const { user, setUser } = useContext(UserDataContext);
-    const UserName = localStorage.getItem('username');
+  
     const onHandleLogout = async () => {
         try {
             setUser(null);
             const response = await axios.post('/api/users/logout');
     
-            localStorage.removeItem('user');
+           
             toast.success(response.data.message);
             router.push('/login');
         } catch (error: any) {
@@ -35,7 +35,7 @@ export default function NavBar() {
                 <img src={logo.src} alt="" className='w-56 md:w-20' />
             </div>
             <div className="profile flex justify-center items-center text-center">
-                <span className="username text-2xl font-bold font-secondary mr-3 md:text-sm">Welcome {UserName || user?.username || user?.name}</span>
+                <span className="username text-2xl font-bold font-secondary mr-3 md:text-sm">Welcome { user?.username || user?.name}</span>
                 <div onClick={onHandleLogout}>
                     <LogoutIcon className='cursor-pointer text-4xl md:text-2xl' />
                 </div>
